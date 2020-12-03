99b86d9f6522a1b18606e1f0a058eae2ae6fdde8
Vagner Felix Bandeira <61794380+Vbandeirarj@users.noreply.github.com>
2020-12-03 20:24:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,147 @@
+
+    var cont=0
+    var acertou=0
+    var errou=0
+    
+    function opt(){
+        
+        var res= form.resp.value
+        if (res=="a"){
+            acertou++
+            alert("Parabéns, voce acertou!!!")
+            
+        }else{
+            errou++
+            alert("Que pena, voce errou essa quetão!!!")
+            
+        }
+
+    }   
+    function opt2(){
+        
+        var res= form.resp.value
+        if (res=="d"){
+            acertou++
+            alert("Parabéns, voce acertou!!!")
+            
+        }else{
+            errou++
+            alert("Que pena, voce errou essa quetão!!!")
+            
+        }
+
+    }
+    function opt3(){
+        
+        var res= form.resp.value
+        if (res=="b"){
+            acertou++
+            alert("Parabéns, voce acertou!!!")
+            
+        }else{
+            errou++
+            alert("Que pena, voce errou essa quetão!!!")
+            
+        }
+
+    }
+    function opt4(){
+        
+        var res= form.resp.value
+        if (res=="b"){
+            acertou++
+            alert("Parabéns, voce acertou!!!")
+            
+        }else{
+            errou++
+            alert("Que pena, voce errou essa quetão!!!")
+            
+        }
+
+    }
+    function opt5(){
+        
+        var res= form.resp.value
+        if (res=="b"){
+            acertou++
+            alert("Parabéns, voce acertou!!!")
+            
+        }else{
+            errou++
+            alert("Que pena, voce errou essa quetão!!!")
+            
+        }
+
+    }
+    function opt6(){
+        
+        var res= form.resp.value
+        if (res=="c"){
+            acertou++
+            alert("Parabéns, voce acertou!!!")
+            
+        }else{
+            errou++
+            alert("Que pena, voce errou essa quetão!!!")
+            
+        }
+
+    }
+    function opt7(){
+        
+        var res= form.resp.value
+        if (res=="a"){
+            acertou++
+            alert("Parabéns, voce acertou!!!")
+            
+        }else{
+            errou++
+            alert("Que pena, voce errou essa quetão!!!")
+            
+        }
+
+    }
+    function opt8(){
+        
+        var res= form.resp.value
+        if (res=="a"){
+            acertou++
+            alert("Parabéns, voce acertou!!!")
+            
+        }else{
+            errou++
+            alert("Que pena, voce errou essa quetão!!!")
+            
+        }
+
+    }
+    function opt9(){
+        
+        var res= form.resp.value
+        if (res=="a"){
+            acertou++
+            alert("Parabéns, voce acertou!!!")
+            
+        }else{
+            errou++
+            alert("Que pena, voce errou essa quetão!!!")
+            
+        }
+
+    }
+    function opt10(){
+        
+        var res= form.resp.value
+        if (res=="c"){
+            acertou++
+            alert("Parabéns, voce acertou!!!, voce teve um total de " + acertou + " acertos, e um total de " + errou + " erros")
+            
+        }else{
+            errou++
+            alert("Que pena, voce errou essa quetão!!!, voce teve um total de " + acertou + " acertos, e um total de " + errou + " erros")
+            
+        }
+
+    }
+    
+    
